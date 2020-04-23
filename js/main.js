387ebb768b84cea30de3f32b087d3811eac2f040
Simone Sala <simonesala@me.com>
2020-04-23 17:29:07 +0200

    Step 2

--- js/main.js
+++ js/main.js
@@ -3,35 +3,31 @@ $(document).ready(function() {
     $.ajax({
         url: "server.php",
         method: "GET",
-        success: function(vendite) {
+        success: function(data) {
+            var orange = "#FFA400";
+            var lightblue = "#009FFD";
+            var blue = "#2A2A72";
+            var black = "#232528";
+            var white = "#EAF6FF";
             var mesi = ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"];
-            var ctx = $("#first-chart");
-            var chartOne = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: mesi,
-                    datasets: [{
-                        label: "Vendite",
-                        backgroundColor: "#009ffd",
-                        borderColor: "#009ffd",
-                        pointBackgroundColor: "#eaf6ff",
-                        data: vendite
+            var noOptions = {};
+            var optionsChart1 = {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 1000
 
+                        }
                     }]
-
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true,
-                                stepSize: 1000
-
-                            }
-                        }]
-                    }
                 }
-            });
+            };
+            makeChart($("#first-chart"), data.fatturato.type, "Vendite", data.fatturato.data, mesi, optionsChart1, lightblue, lightblue, white);
+            var venditori = Object.keys(data.fatturato_by_agent.data);
+            var vendite = Object.values(data.fatturato_by_agent.data);
+            makeChart($("#second-chart"), data.fatturato_by_agent.type, "Risultati Venditori", vendite, venditori, noOptions, [lightblue, blue, orange, black], white);
+
+
         },
         error: function(err) {
             alert("Errore: " + err);
@@ -39,8 +35,25 @@ $(document).ready(function() {
     });
 
 
+    function makeChart(idChart, type, legenda, data, labels, options, bgColor, bdColor, otherColor) {
+        var ctx = idChart;
+        var chart = new Chart(ctx, {
+            type: type,
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: legenda,
+                    backgroundColor: bgColor,
+                    borderColor: bdColor,
+                    pointBackgroundColor: otherColor,
+                    data: data
 
+                }]
 
+            },
+            options: options
 
+        });
+    }
 
 }); //==========
--- server.php
+++ server.php
@@ -1,5 +1,7 @@
 <?php
+
     include "data.php";
     header("Content-Type: application/json");
     echo $database;
+
 ?>
